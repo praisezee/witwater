36f5ec75729367872f700f05d95a20923c3edd0b
Folorunso Praise <folorunsopraise580@gmail.com>
2023-04-28 09:50:09 +0100

    no updates

--- src/Components/api/register.js
+++ src/Components/api/register.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-//const BASE_URL = 'https://witwater-server.onrender.com'
+const BASE_URL = 'https://witwater-server.onrender.com'
 const DEV_SERVER = "http://localhost:3500"
 
 export default axios.create( {
-      baseURL: DEV_SERVER
+      baseURL: BASE_URL
 })
