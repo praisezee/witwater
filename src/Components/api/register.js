44323c74ec9c0d569b66d057a70df9f1e3557ced
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2023-04-30 06:56:54 +0100

    Update register.js
    now using deployed url

--- src/Components/api/register.js
+++ src/Components/api/register.js
@@ -4,10 +4,10 @@ const BASE_URL = 'https://witwater-server.onrender.com'
 const DEV_SERVER = "http://localhost:3500"
 
 export default axios.create( {
-      baseURL: DEV_SERVER
+      baseURL: BASE_URL
 })
 export const axiosPrivate = axios.create( {
-      baseURL: DEV_SERVER,
+      baseURL: BASE_URL,
       headers: { 'Content-Type': 'application/json' },
       withCredentials: true
 })
