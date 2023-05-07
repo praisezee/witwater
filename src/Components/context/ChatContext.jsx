1318e7e9b610671337ae4018d3cada46de9689d1
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2023-05-07 23:10:14 +0100

    Update ChatContext.jsx

--- src/Components/context/ChatContext.jsx
+++ src/Components/context/ChatContext.jsx
@@ -105,7 +105,7 @@ export const ChatProvider = ( { children } ) =>
     const receiverId = currentChat.members.find(member => member !== auth.id)
 
     socket.current.emit( 'sendMessage', {
-      sender: auth.id,
+      senderId: auth.id,
       receiverId,
       text: newMessage
     })
