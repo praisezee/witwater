cc6fa77d74d36086725dd91946949b8ca0bc1cf9
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2023-05-28 20:26:30 +0100

    Update MyPost.jsx

--- src/Components/Dashboard/MyPost.jsx
+++ src/Components/Dashboard/MyPost.jsx
@@ -11,10 +11,10 @@ const MyPost = ( { post, auth } ) =>
     <Col key={ post._id } xs={ 11 } className='border mx-auto rounded shadow-sm bg-body-tertiary rounded rounded-3 my-3 py-2'>
       <div className="d-flex">
         <div className="rounded-circle w-5 mx-auto border border-info">
-          <img src={auth.src !== '' ? auth.src :auth.src === '' && auth.gender.toLowerCase() === 'male' ? male : auth.src === '' && auth.gender.toLowerCase() === 'female'? female : null} alt="" className='img-fluid w-100 rounded-circle' />
+          <img src={auth.src !== '' ? auth?.src :auth?.src === '' && auth?.gender.toLowerCase() === 'male' ? male : auth?.src === '' && auth?.gender.toLowerCase() === 'female'? female : null} alt="" className='img-fluid w-100 rounded-circle' />
         </div>
         <div className="flex-grow-1 my-auto ms-2">
-          <p className="h6 text-capitalize">{ auth.name }</p>
+          <p className="h6 text-capitalize">{ auth?.name }</p>
         </div>
       </div>
       <Link to={`../post/${post._id}`} className='nav-link'>
