65928ad39ce16407b14c564ada5d6b33519ea8b7
Apus Industries <folorunsopraise580@gmail.com>
2023-05-28 15:38:29 +0100

    register button fixed

--- src/Components/api/register.js
+++ src/Components/api/register.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://witwater-server.onrender.com'
-const DEV_SERVER = "http://localhost:3500"
+//const DEV_SERVER = "http://localhost:3500"
 
 export default axios.create( {
-      baseURL: DEV_SERVER
+      baseURL: BASE_URL
 })
 export const axiosPrivate = axios.create( {
-      baseURL: DEV_SERVER,
+      baseURL: BASE_URL,
       headers: { 'Content-Type': 'application/json' },
       withCredentials: true
 })
