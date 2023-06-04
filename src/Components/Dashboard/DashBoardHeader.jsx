5e1bc09ebbc1fd2ff2379e971b237122a503f061
Apus Industries <folorunsopraise580@gmail.com>
2023-06-04 08:38:55 +0100

    rectified

--- src/Components/Dashboard/DashBoardHeader.jsx
+++ src/Components/Dashboard/DashBoardHeader.jsx
@@ -16,55 +16,58 @@ const DashBoardHeader = () =>
     navigate('../auth/login')
   }
   return (
-      <Navbar collapseOnSelect  bg="light" className='py-0 px-0 w-100 mx-0 height-50' expand='sm'>
-          <Container fluid>
-            <Navbar.Brand className='f-percifico'><Link to='../dashboard' className='nav-link d-flex'>
-            <p className=" h3 f-percifico">Witwater</p>
-          </Link></Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar"/>
-            <Navbar.Offcanvas
-            placement="end"
-            id="responsive-navbar"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title >
-                  Menu
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1">
-                <NavLink eventKey={ 1 } className='mx-2'>
-                  <Link className='nav-link text-secondary d-flex' to='/dashboard'>
-                    <BsHouse className='text-dark d-none my-auto d-md-block'/>
-                    <span className='mx-1 my-auto'>Home</span>
-                  </Link>
-                </NavLink>
-                <NavLink eventKey={ 1 } className='mx-2'>
-                  <Link className='nav-link text-secondary d-flex' to='new-post'>
-                    <BsPlusLg className='text-dark d-none my-auto d-md-block'/>
-                    <span className='mx-1 my-auto'>New Post</span>
-                  </Link>
-                </NavLink>
-                <NavLink eventKey={3} className='mx-2'>
-                  <Link className='nav-link text-secondary d-flex' to='chat'>
-                    <BsChat className='text-dark d-none my-auto d-md-block'/>
-                    <span className='mx-1 my-auto'>Chats</span>
-                  </Link>
-                </NavLink>
-                <NavLink eventKey={4} className='mx-2'>
-                  <Link className='nav-link text-secondary d-flex' to='profile'>
-                    <BsPersonCircle className='text-dark d-none my-auto d-md-block'/>
-                    <span className='mx-1 my-auto'>Profile</span>
-                  </Link>
-                </NavLink>
-                <div className="my-auto">
-                  <Button variant='outline-primary' onClick={signOut}>Sign out</Button>
-                </div>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
+  <Navbar collapseOnSelect  bg="light" className='py-0 px-0 w-100 mx-0' expand='md'>
+    <Container fluid>
+      <Navbar.Brand className='f-percifico'>
+        <Link to='../dashboard' className='nav-link d-flex'>
+          <p className=" h4 f-percifico">Witwater</p>
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='offcanvasNavbar-expand-lg'/>
+      <Navbar.Offcanvas
+        id='offcanvasNavbar-expand-lg'
+        aria-labelledby="offcanvasNavbarLabel-expand-lg"
+        placement="end"
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title >
+            Menu
+          </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body className='justify-content-end'>
+          <Nav className="">
+            <NavLink eventKey={ 1 } >
+              <Link className='nav-link text-secondary d-flex' to='/dashboard'>
+                <BsHouse className='text-dark d-none my-auto d-md-block'/>
+                <span className='ms-1 my-auto small'>Home</span>
+              </Link>
+            </NavLink>
+            <NavLink eventKey={ 1 } >
+              <Link className='nav-link text-secondary d-flex' to='new-post'>
+                <BsPlusLg className='text-dark d-none my-auto d-md-block'/>
+                <span className='ms-1 my-auto small'>New Post</span>
+              </Link>
+            </NavLink>
+            <NavLink eventKey={3} >
+              <Link className='nav-link text-secondary d-flex' to='chat'>
+                <BsChat className='text-dark d-none my-auto d-md-block'/>
+                <span className='ms-1 my-auto small'>Chats</span>
+              </Link>
+            </NavLink>
+            <NavLink eventKey={4} >
+              <Link className='nav-link text-secondary d-flex' to='profile'>
+                <BsPersonCircle className='text-dark d-none my-auto d-md-block'/>
+                <span className='ms-1 my-auto small'>Profile</span>
+              </Link>
+            </NavLink>
+          </Nav>
+          <div className='my-auto'>
+            <Button variant='outline-primary' className='my-auto' onClick={signOut} size='sm'>Sign out</Button>
+          </div>
+        </Offcanvas.Body>
+      </Navbar.Offcanvas>
+    </Container>
+  </Navbar>
   )
 }
 
